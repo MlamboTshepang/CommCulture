3ab6811a2c859d49f33c3558a51a9539d0b04533
MlamboTshepang <Tshepangmlambo@gmail.com>
2026-03-04 09:42:29 +0200

    Maintenance
    Maintenance of the Footer, Header and STtyling of the page. The navigation bar was also attended to

--- CommCulture/Index.js
+++ CommCulture/Index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('About.ejs');
 });
 app.listen(port, () => {
     console.log('Listening on port', port);
